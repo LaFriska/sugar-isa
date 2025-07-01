e32b5d8fcedec2118fef2fb3e67e316bcda79dc8
LaFriska <harold.gao205@gmail.com>
2025-07-01 21:04:08 +1000

    Added architectural state interface

---
+++ src/main/java/ArchitecturalState.java
@@ -0,0 +1,29 @@
+import xyz.haroldgao.sugarisa.Register;
+
+/**
+ * Represents the architectural state, along with getters and setters for various parameters.
+ * */
+public interface ArchitecturalState {
+
+    /**
+     * Reads a value from a register.
+     * @see Register
+     * */
+    int read(Register register);
+
+    /**
+     * Writes to a register.
+     * @see Register
+     * */
+    void write(Register register, int value);
+
+    /**
+     * Reads from a memory address.
+     * */
+    int read(int address);
+
+    /**
+     * Writes to a memory address.
+     * */
+    int write(int address, int value);
+}
