4e032ee52466cde22e16f328764ca4d178c2dd52
LaFriska <harold.gao205@gmail.com>
2025-07-01 23:23:47 +1000

    Added instruction class

--- src/main/java/Instruction.java
+++ src/main/java/Instruction.java
@@ -2,4 +2,8 @@
 /**
  * Represents an arbitrary instruction.
  * */
-public abstract class Instruction {}
+public abstract class Instruction {
+
+    public abstract void execute(ArchitecturalState state);
+
+}
