d9d3fdad5b185276a33c123b48072406ef8dbeca
Harold Gao <harold.gao205@gmail.com>
2025-07-18 22:20:02 +1000

    added template for new parser

---
+++ src/main/java/xyz/haroldgao/sugarisa/parser/Linker.java
@@ -0,0 +1,14 @@
+package xyz.haroldgao.sugarisa.parser;
+
+import xyz.haroldgao.sugarisa.tokeniser.Token;
+
+import java.util.HashMap;
+import java.util.List;
+
+class Linker {
+
+    protected static HashMap<String, Integer> link(List<List<Token>> tokens){
+        return null; //TODO
+    }
+
+}
--- src/main/java/xyz/haroldgao/sugarisa/parser/Parser.java
+++ src/main/java/xyz/haroldgao/sugarisa/parser/Parser.java
@@ -1,43 +1,20 @@
 package xyz.haroldgao.sugarisa.parser;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import xyz.haroldgao.sugarisa.execute.instructions.Instruction;
 import xyz.haroldgao.sugarisa.tokeniser.Token;
 import xyz.haroldgao.sugarisa.tokeniser.TokenType;
 import xyz.haroldgao.sugarisa.tokeniser.Tokeniser;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.HashMap;
 import java.util.List;
-
-import static xyz.haroldgao.sugarisa.tokeniser.TokenType.*;
 
 /**
  * Parser for sugar assembly. Takes a {@link Tokeniser} as input and returns a series of
  * {@link Instruction}s.
  * */
-public class Parser implements Iterator<Instruction> {
-
-    private @NotNull final Tokeniser tokeniser;
-
-    protected @NotNull final String assembly;
-
-    private @Nullable Token buffer;
-
-    private int nextInstructionAddress = 0;
-
-    private ParseState parseState;
-
-    private static final ParseTree PARSE_TREE = SugarParseTree.get();
-
-
-    protected Parser(@NotNull Tokeniser tokeniser){
-        this.tokeniser = tokeniser;
-        this.assembly = tokeniser.getBuffer();
-        nextToken();
-        processComments();
-    }
+public class Parser {
 
     /**
      * Parses assembly code into a series of instructions.
@@ -45,144 +22,29 @@ public class Parser implements Iterator<Instruction> {
      * @throws ParseError if the assembly is semantically incorrect.
      * */
     public static List<Instruction> parse(@NotNull String assembly){
-       ArrayList<Instruction> instructions = new ArrayList<>();
-       Parser p = new Parser(new Tokeniser(assembly));
-       while(p.hasNext()){
-           instructions.add(p.next());
+       Tokeniser tokeniser = new Tokeniser(assembly);
+       List<List<Token>> partition = new ArrayList<>();
+       while(tokeniser.hasNext()){
+           ArrayList<Token> current = new ArrayList<>();
+           while(tokeniser.hasNext()){
+               Token t = tokeniser.next();
+               if(t.type() == TokenType.COMMENT) continue; //ignore comments
+               current.add(t);
+               if(t.type() == TokenType.TERM)
+                   break;
+           }
+           partition.add(current);
        }
-       return instructions;
-    }
-
-
-    /**
-     * Checks whether there are more tokens to be parsed.
-     * */
-    /*
-    * requires this.buffer == null || this.buffer.type != TokenType.COMMENT
-    * */
-    @Override
-    public boolean hasNext() {
-        return this.buffer != null;
-    }
-
-    /**
-     * Gets the next instruction.
-     * */
-    /*
-     * requires: this.buffer == null || this.buffer.type != TokenType.COMMENT
-     * requires: the current buffer to hold the first token of the next <part> token.
-     * ensures: this.buffer == null || this.buffer.type != TokenType.COMMENT
-     * */
-    @Override
-    public Instruction next() {
-        if(!hasNext()) throw new NoMoreInstructionsError(this,
-                "The given assembly code is either fully parsed or contains no tokens.");
-
-        if(buffer.type() == LABEL) processLabel(buffer.value());
-        return parseInstruction();
-    }
-
-    /**
-     * Given that the parser currently points toward the first token of the next instruction,
-     * parses the next instruction via the following CFG. This method also processes the terminator token.
-     * */
-    private Instruction parseInstruction(){ //<instruction> := <duo> | <solo> | <simple> | ε
-        parseState = new ParseState();
-        ParseTree current = PARSE_TREE;
-        while(!current.isLeaf()){
-            current = current.run(buffer, parseState);
-            if(buffer == null) throw new UnfinishedInstructionException(this);
-            if(current == null) throw new UnexpectedTokenError(this, buffer);
-            nextToken();
+       HashMap<String, Integer> linker = Linker.link(partition);
+       ArrayList<Instruction> result = new ArrayList<>();
+        for (List<Token> tokens : partition) {
+            result.add(parseSingleInstruction(tokens, linker));
         }
-        //current.isLeaf() is true. All leafs should have equivalence predicate on the terminator.
-        //Here, the buffer being at the start of the next instruction is ensured.
-        return returnInstruction(current.getInstruction(parseState));
+        return result;
     }
 
-    /**
-     * Processes a label by registering the next instruction as the value of the label.
-     * @throws DuplicateLabelException if the label already exists.
-     * @throws UnclosedLabelException if the label does not end in a colon.
-     * */
-    private void processLabel(String label){
-        if(parseState.hasLabel(label)) throw new DuplicateLabelException(this, label);
-        nextToken();
-        if(!testType(buffer, COLON)) throw new UnclosedLabelException(this, label);
-        parseState.addLink(label, nextInstructionAddress);
-        nextToken();
-    }
-
-    /**
-     * Null-safe check of the type of an arbitrary token.
-     * */
-    private static boolean testType(Token t, @NotNull TokenType type){
-        if(t == null) return false;
-        return t.type() == type;
-    }
-
-    /**
-     * Stores the next token in the tokeniser in a buffer. If the tokeniser
-     * has no more tokens, sets the buffer to null. This method also skips comments by
-     * calling {@link Parser#processComments()}.
-     * @return the previous token held in the buffer.
-     * */
-    private Token nextToken(){
-        var oldBuffer = buffer;
-        if(tokeniser.hasNext()){
-            buffer = tokeniser.next();
-        }else{
-            buffer = null;
-        }
-        processComments();
-        return oldBuffer;
-    }
-
-    /**
-     * Recursively move the buffer to the next token that is not a comment.
-     * */
-    private void processComments(){
-        if(buffer != null && buffer.type() == COMMENT){
-            buffer = tokeniser.hasNext() ? tokeniser.next() : null;
-            processComments();
-        }
-    }
-
-    private Instruction returnInstruction(Instruction in){
-        nextInstructionAddress += 4;
-        return in;
-    }
-
-
-    /**
-     * Parses an immediate value.
-     * @param imm String representation of the immediate.
-     * @param maxbits maximum number of bits allowed.
-     * @param radix the power base of the string representation of the immediate.
-     * @throws OversizedImmediateError if the resulting immediate is too large.
-     * */
-    @Deprecated
-    protected int parseImmediate(@NotNull String imm, int maxbits, int radix) {
-
-        int value;
-        try {
-            value = Integer.parseUnsignedInt(imm, radix);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("The input string does not represent an immediate value. ");
-        }
-
-        if (value >> maxbits != 0)
-            throw new OversizedImmediateError(this, imm, maxbits);
-
-        return value;
-    }
-
-    protected int signExtend(int imm, int immSize) {
-        int signBit = immSize - 1;
-        if (signBit == 31) return imm;
-
-        int shift = 31 - signBit;
-        return (imm << shift) >> shift;
+    private static Instruction parseSingleInstruction(@NotNull List<Token> tokens, @NotNull HashMap<String, Integer> linker){
+        return null; //TODO
     }
 
 }
