bab69cfc2292dc525b8c20cb4263d6633e9aa629
LaFriska <harold.gao205@gmail.com>
2025-07-27 15:39:49 +1000

    Added terminator to the loader

--- src/main/java/xyz/haroldgao/sugarisa/execute/Loader.java
+++ src/main/java/xyz/haroldgao/sugarisa/execute/Loader.java
@@ -1,6 +1,7 @@
 package xyz.haroldgao.sugarisa.execute;
 
 import xyz.haroldgao.sugarisa.execute.instructions.Instruction;
+import xyz.haroldgao.sugarisa.execute.instructions.Terminator;
 
 /**
  * A loader takes a series of instructions and converts it to a memory instance with
@@ -13,12 +14,14 @@ class Loader {
     private Loader(){}
 
     ArchitecturalState load(Instruction[] instructions){
-        int pc;
+        int pc = -4;
         Memory memory = new Memory();
         for(int i = 0; i < instructions.length; i++){
             pc = i * 4;
             memory.writeWord(pc, instructions[i].getBinary());
         }
+        memory.writeWord(pc + 4, Terminator.getInstance().getBinary());
+
         return new Microarchitecture(memory, new RegisterFile());
     }
 
